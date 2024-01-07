b4cc8cb93b4a75dab0e983f5d9dd80e831f8b852
NyH1409 <hei.nyhasina@gmail.com>
2024-01-07 13:18:31 +0300

    chore: setup tailwindcss conf

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import { useEffect, useId, useRef, useState } from "react";
 
 function App() {
-  const [name, setName] = useState("")
-  const reference = useRef()
-  useId()
-  useEffect(()=>{
-    console.log(name);
-  }, [name])
-
   return (
-    <div className="App">
+    <div className="text-3xl font-bold underline">
       <p>Picksary</p>
-      <input type="text" name="name" onChange={e => setName(e.target.value)}/>
-      <div>
-        { name }
-      </div>
-      <input type="text" name="reference" ref={reference}/>
     </div>
   );
 }
