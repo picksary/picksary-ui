6c830f87e9dabde4c7ac6ceb690ae2059d33169b
NyH1409 <hei.nyhasina@gmail.com>
2024-01-07 22:32:01 +0300

    feat: create banner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css"
 import { Menu } from "./app/menu/menu";
+import { Search } from "./app/search/search";
+import { Blank } from "./utils/utils";
 
 function App() {
 
@@ -19,9 +21,17 @@ function App() {
     <div>
       <div className="banner">
         <Menu scrollHeight={scrollHeight}/>
+        <Blank/>
+        <div className="text-white text-2xl font-bold text-center pt-12 pb-4">
+            <h2>Telecharger librement et gratuitement des contenus sur Madagascar</h2>
+            <p className="text-sm/[10px] font-light pt-2">Plus de details sur la <a className="underline">Licence des contenus</a></p>
+        </div>
+        <div style={{ width: "1000px", margin: "auto" }}>
+          <Search />
+        </div>
       </div>
       <div>
-
+     
       </div>
     </div>
   );
--- src/app/search/search.js
+++ src/app/search/search.js
@@ -7,7 +7,7 @@ export const Search = ( { scrollHeight }) => {
             <div className="flex" style={{ display: scrollHeight < 50 ? 'none' : "block",
         transition: "0.25s ease-in-out" }}>
                 <div className="relative w-full">
-                    <input type="search" id="search-dropdown" className="font-italic block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Recherche dans PickSary" required/>
+                    <input type="search" id="search-dropdown" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-e-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Recherche dans PickSary" required/>
                     <button type="submit" className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-violet-800 rounded-e-lg border border-violet-800 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-violet-300 dark:bg-violet-600 dark:hover:bg-violet-700 dark:focus:ring-violet-800">
                         <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
