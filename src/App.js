04ae1a115029e24fc4c72cc9412896a92577aa1c
NyH1409 <hei.nyhasina@gmail.com>
2024-01-07 21:32:49 +0300

    feat: create navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react";
+import "./App.css"
+import { Menu } from "./app/menu/menu";
 
 function App() {
+
+  const [scrollHeight, setScrollHeight] = useState("none")
+
+  useEffect(()=>{
+      const handleScroll = ()=> {
+          setScrollHeight(window.scrollY)
+          console.log(window.scrollY);
+      }
+      window.addEventListener('scroll', handleScroll)
+      return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
-    <div className="text-3xl font-bold underline">
-      <p>Picksary</p>
+    <div>
+      <div className="banner">
+        <Menu scrollHeight={scrollHeight}/>
+      </div>
+      <div>
+
+      </div>
     </div>
   );
 }
