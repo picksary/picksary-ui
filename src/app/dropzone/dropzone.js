c81168c0622655fc2490745a0276be69f722d09c
NyH1409 <hei.nyhasina@gmail.com>
2024-01-12 20:50:15 +0300

    chore: dropzone for drag and drop

--- src/app/dropzone/dropzone.js
+++ src/app/dropzone/dropzone.js
@@ -1,6 +1,28 @@
+import { useCallback, useState } from "react"
+import { useDropzone } from "react-dropzone"
+
+
 export const DragAndDropZone = ()=>{
+    const [folder, setFolder] = useState([])
+    const onDrop = useCallback((files)=>{
+        const file = files[0]
+        console.log(file);
+        if (file && file.webkitRelativePath) {
+            const folderName = file.webkitRelativePath.split('/')[0];
+            setFolder(folderName)
+        }
+    }, [])
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop, 
+        multiple: false, 
+        directory: true
+    })
+
     return (
-        <>
-        </>
+        <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p>Drop folder here</p>
+        </div>
     )
 }
