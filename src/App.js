1eeddbd9b540082b364bd2b55d9f3d365744b0c8
NyH1409 <hei.nyhasina@gmail.com>
2024-01-14 16:46:09 +0300

    chore: setup routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { Route, Routes } from "react-router";
 import "./App.css"
 import { Profile } from "./app/profile/profile";
+import { Home } from "./app/home/home"
 
 function App() {
 
   return (
    <>
-    <Profile />
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/show" element={<Profile/>}></Route>
+      </Routes>
    </>
   );
 }
